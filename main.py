673de912600e4490e0cca8393f141eb6e9c848ea
anatom <49984698+anatom3000@users.noreply.github.com>
2021-09-23 16:28:03 +0000

    2nd rewrite of the engine (not to be faster, just to make it actually work) (data-driven go brrrr)

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import tkinter
-from page import Page
+import tkinter.font
+#from page import Page
 
 root = tkinter.Tk()
 
-webpage = Page(root)
-webpage.pack()
+file = "test.html"
+
+f = tkinter.font.Font(family="Helvetica", size=12)
+
+l = tkinter.Label(root, text=file)
+l.pack()
+
+f = tkinter.font.nametofont(l["font"])
+for i in ("family","size","weight", "slant", "underline", "overstrike"):
+    print(i, repr(f[i]))
+
+"""webpage = Page(root, file)
+webpage.pack()"""
 
 root.mainloop()
--- page.py
+++ page.py
@@ -1,6 +1,34 @@
+import json
+import os
 import tkinter as tk
+import bs4
+
+NODES = {}
+files = [i if os.path.isfile("assets/nodes/"+i) for i in os.listdir('assets/nodes')]
+
+class Node:
+    def __init__(self, master, attrs, children, extras={}):
+        self.master = master
+        self.attrs = attrs
+        self.children = children
+        self.extras = extras
+
+        self.widget = master
+    
+    def pack(self, **kwargs):
+        pass
+
 
 class Page(tk.Frame):
-    def __init__(self, window):
+    def __init__(self, window, file):
         super().__init__(window)
         self.window = window
+        self.parse(file)
+
+    def parse(self, file):
+        with open(file) as f:
+            self.html_tree = bs4.BeautifulSoup()
+        for child in self.html_tree.children:
+            pass
+
+
